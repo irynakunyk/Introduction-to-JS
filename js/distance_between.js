d5266e9f673ab12ce5b359ff033e1884596fbde1
irynakunyk <iryna.kunyk@gmail.com>
2016-04-26 21:16:17 +0300

    Create distance_between.js

---
+++ js/distance_between.js
@@ -0,0 +1,7 @@
+var x1 = prompt("please enter x1","Default value");
+var y1 = prompt("please enter y1","Default value");
+var x2 = prompt("please enter x2","Default value");
+var y2 = prompt("please enter y2","Default value");
+var a = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2))
+var b = a.toFixed(2);
+alert(b);
